8662a76900664402261a829bfa2927150ebe2879
Ameco <hamecow@gmail.com>
2020-09-16 11:38:01 -0700

    Delete unused code from server.js

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
+//PORT
+const PORT = process.env.PORT || 3000;
+
 //Dependencies
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const noteText = require('./db/db.json');
-//const noteIndex = [];
-
-
 
 //Express App
 const app = express();
-const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/db'));
-//app.use(express.static('public'));
-//app.use(express.static('db'));
-//app.use('/static', express.static(path.join(__dirname, 'public')));
-//app.use('/static', express.static(path.join(__dirname, 'db')));
 
 //Express Data Parsing
 app.use(express.urlencoded({ extended: true }));
@@ -30,19 +25,12 @@ app.get('/notes', function(req, res) {
 });
 
 app.get('/api/notes', function(req, res) {
-    //res.send(database);
-    //res.sendFile(path.join(__dirname, '/db/db.json'));
-    //return res.json(notes);
     res.json(noteText);
 });
 
 //POST
 app.post('/api/notes', function(req, res) {
-    //noteIndex.push(req.body);
-    //noteText.push(noteIndex);
     noteText.push(req.body);
-    //console.log(noteData);
-    //noteText.push(req.body);
     fs.writeFile('db/db.json', JSON.stringify(noteText), function(err) {
         if (err) {
             return console.log(err);
@@ -74,35 +62,7 @@ app.delete('/api/notes/:id', function(req, res) {
     res.json(noteText);
 });
 
-
-
-
-// noteText.forEach((note) => {
-//     if (note.id === paramId) {
-//         const noteIndex = noteText.indexOf(note);
-//         noteText.splice(noteIndex, 1);
-//     }
-// });
-
-// fs.writeFile('db/db.json', JSON.stringify(noteText), function(err) {
-//     if (err) {
-//         return console.log(err);
-//     }
-// });
-
-//res.json(noteText);
-//});
-
 //Listen
 app.listen(PORT, function() {
     console.log('App listening on PORT ' + PORT);
 });
-
-//Append File
-
-//function addToFile() {
-//fs.appendFile('/db/db.json', newNote, function(err) {
-// if (err) return console.log(err);
-//  console.log('Appended!');
-////});
-//}
